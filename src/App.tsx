fd5b929017bddd68b60b3192b4f20ca401e82897
Vani Yaji <vaniyaji@Vanis-MacBook-Pro.local>
2019-01-24 12:10:45 +0530

    React bootstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Button variant="primary">Primary</Button>
       </div>
     );
   }
